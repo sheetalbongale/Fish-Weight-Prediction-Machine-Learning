340046313e6ae3f105d982327707fa0e5fb3f5f1
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2020-05-10 19:21:11 -0500

    Update better fit by changing max_depth

--- train.py
+++ train.py
@@ -4,16 +4,15 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from preprocess import prep_data
-
 # ------------------ sci-kit learn ------------------#
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
-
 # ----------------- joblib dump & load --------------#
 from joblib import dump
 from joblib import load
+#=====================================================#
 
 
 # ============= Load Data $ Train Model ==============#
@@ -23,7 +22,7 @@ df = pd.read_csv(csv_path)
 X, y = prep_data(df)
 
 gbr = GradientBoostingRegressor(
-    n_estimators=100, max_depth=6, min_samples_split=2, learning_rate=0.1, loss="ls",
+    n_estimators=100, max_depth=8, min_samples_split=2, learning_rate=0.1, loss="ls",
 )
 
 gbr.fit(X, y)
